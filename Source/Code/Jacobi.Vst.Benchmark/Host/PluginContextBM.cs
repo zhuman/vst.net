cf1215dd5cff1383d5f509783df8eb80e147302f
Marc Jacobi <marc.jacobi@macaw.nl>
2020-10-02 08:44:53 +0200

    add proj

---
+++ Source/Code/Jacobi.Vst.Benchmark/Host/PluginContextBM.cs
@@ -0,0 +1,12 @@
+﻿using Jacobi.Vst.Host.Interop;
+
+namespace Jacobi.Vst.Benchmark.Host
+{
+    internal class PluginContextBM
+    {
+        public void CreateContext()
+        {
+            VstPluginContext.Create()
+        }
+    }
+}
---
+++ Source/Code/Jacobi.Vst.Benchmark/Program.cs
@@ -0,0 +1,13 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace Jacobi.Vst.Benchmark.Plugin
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Host
+            BenchmarkRunner.Run<Host.PluginContextBM>();
+        }
+    }
+}
