6ee4dad65dd3adad63fd1cfb95b176cd04d1b2dd
Zachary Northrup <z-systems@hotmail.com>
2020-12-27 01:54:24 -0500

    Small VST3 tweaks to get it building correctly with .NET 5

--- Source3/Code/Jacobi.Vst3.Interop/dllmain.cpp
+++ Source3/Code/Jacobi.Vst3.Interop/dllmain.cpp
@@ -1,12 +1,6 @@
 #include "pch.h"
 #include <delayimp.h>
 
-//HMODULE g_hModule;
-
-//char buffer[MAX_PATH + 1];
-// Get full path to ijwhost.dll based on current module path
-//auto res = GetModuleFileNameA(g_hModule, buffer, MAX_PATH + 1);
-
 #pragma unmanaged
 
 extern "C"
@@ -26,22 +20,6 @@ extern "C"
         }
         return TRUE;
     }*/
-
-    FARPROC WINAPI delayLoadFailureHook(unsigned dliNotify, PDelayLoadInfo pdli)
-    {
-        if (dliNotify != dliFailLoadLib ||
-            _strcmpi(pdli->szDll, "ijwhost.dll") != 0) {
-            return NULL;
-        }
-
-
-        ::OutputDebugStringA("Loading Ijwhost.dll");
-
-        const char* path = "C:\\Users\\marc\\Documents\\MyProjects\\public\\Jacobi\\Public\\GitHub\\vst.net\\Source3\\Code\\x64\\Debug\\Ijwhost.dll";
-        return (FARPROC)::LoadLibraryA(path);
-    }
-
-    const PfnDliHook __pfnDliFailureHook2 = delayLoadFailureHook;
 }
 
 #pragma managed
